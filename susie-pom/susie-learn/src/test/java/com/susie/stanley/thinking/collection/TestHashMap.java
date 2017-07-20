e21281217b560689acf5bee0eb65ded28f661b6e
stanleylus <stanleylus@126.com>
2017-07-21 01:00:19 +0800

    collection-hashmap
    build hashmap by myself

--- susie-pom/susie-learn/src/test/java/com/susie/stanley/thinking/collection/TestHashMap.java
+++ susie-pom/susie-learn/src/test/java/com/susie/stanley/thinking/collection/TestHashMap.java
@@ -2,6 +2,7 @@ package com.susie.stanley.thinking.collection;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 import org.junit.Test;
 
@@ -32,6 +33,27 @@ public class TestHashMap {
 		
 		System.out.println(map.get("2"));
 		
+	}
+	
+	/**
+	 * 用map记录随机数产生次数
+	 */
+	@Test
+	public void testRepeatRecord(){
+		Random rand = new Random(47);
+		Map<Integer, Integer> m = new HashMap<Integer, Integer>();
+		for(int i=0; i<10000; i++){
+			int key = rand.nextInt(128);
+			Integer v = m.get(key);
+			int value = v == null ? 1 : v+1;
+			m.put(key, value);
+//			if(m.containsKey(key)){
+//				m.put(key, m.get(key)+1);
+//			}else{
+//				m.put(key, 1);
+//			}
+		}
+		System.out.println(m);
 	}
 
 }
