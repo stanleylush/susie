641c457bbd750715500b0f7e3407975670ad8b93
stanleylu <stanleylus@126.com>
2017-07-29 11:44:00 +0800

    susie-db
    create susie db model

---
+++ susie-pom/susie-db/src/main/java/org/susie/db/App.java
@@ -0,0 +1,13 @@
+package org.susie.db;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
