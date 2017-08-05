96586552004725e32507099907488a953f39e546
stanleylus <stanleylus@126.com>
2017-08-05 20:30:06 +0800

    work problem
    work problem test

---
+++ susie-pom/susie-learn/src/test/java/com/susie/stanley/word/test/SomeTest.java
@@ -0,0 +1,42 @@
+package com.susie.stanley.word.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class SomeTest {
+	
+	@Test
+	public void testEachListNull(){
+		List<String> strs = null;
+		for(String str: strs){
+			System.out.println(str);
+		}
+	}
+	
+	@Test
+	public void testEachListAddNull(){
+		List<String> strs = new ArrayList<String>();
+		strs.add(null);
+		for(String str: strs){
+			System.out.println(str);
+		}
+	}
+	
+	@Test
+	public void testEachListSize0(){
+		List<String> strs = new ArrayList<String>();
+		for(String str: strs){
+			System.out.println(str);
+		}
+	}
+	
+	@Test
+	public void testStringEquals(){
+		String a = "1";
+		System.out.println(a.equals("1"));
+		System.out.println(a.equals(null));
+	}
+
+}
